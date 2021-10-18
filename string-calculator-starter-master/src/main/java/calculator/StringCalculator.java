a7851190cead8d0bcfb5e160fb3e1418c418cd05
Prasanna Paigude <prasannapaigude2506@gmail.com>
2021-10-18 11:05:37 +0530

    fourth commit

--- string-calculator-starter-master/src/main/java/calculator/StringCalculator.java
+++ string-calculator-starter-master/src/main/java/calculator/StringCalculator.java
@@ -3,7 +3,28 @@ package calculator;
 public class StringCalculator {
 
     public int add(String input) {
-        
+    	String[] numbers = input.split(",");
+    	
+    	if(isEmpty(input)) {
         	return 0;
+    	}
+    	if(input.length() == 1) {
+        	return stringToInt(input);
         }
+        else {
+        	return getSum(numbers[0], numbers[1]);
+        }
+    }
+    	
+    	private int getSum(String numA, String numB) {
+        	return Integer.parseInt(numA) + Integer.parseInt(numB);
+        }
+    
+    private boolean isEmpty(String input) {
+    	return input.isEmpty();
+    }
+    
+    private int stringToInt(String input) {
+    	return Integer.parseInt(input);
+    }
 }
--- string-calculator-starter-master/src/test/java/calculator/StringCalculatorShould.java
+++ string-calculator-starter-master/src/test/java/calculator/StringCalculatorShould.java
@@ -4,10 +4,22 @@ import static org.junit.Assert.assertEquals;
 import org.junit.jupiter.api.Test;
 
 class StringCalculatorShould {
+	
     @Test
     public void empty_string_should_return_0() {
     	StringCalculator stringCalculator = new StringCalculator();
         assertEquals(stringCalculator.add(""), 0);
     }
+    
+    @Test
+    public void string_with_single_number_should_return_number_as_int() {
+    	StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(stringCalculator.add("1"), 1);
+    }
    
+    @Test
+    public void two_numbers_comma_delimited_return_sum() {
+    	StringCalculator stringCalculator = new StringCalculator();
+    	assertEquals(stringCalculator.add("2,3"), 5);
+    }
 }
